dd6a414f421df60c9bc4a2e77da3fad7e45e8f9c
CrunchyPancakes <30matthewt@gmail.com>
2019-11-13 14:09:28 +1300

    Create web-crawling-test.r

---
+++ Web Crawling Practice/web-crawling-test.r
@@ -0,0 +1,72 @@
+# General-purpose data wrangling
+library(tidyverse)  
+
+# Parsing of HTML/XML files  
+library(rvest)    
+
+# String manipulation
+library(stringr)   
+
+# Verbose regular expressions
+library(rebus)     
+
+# Eases DateTime manipulation
+library(lubridate)
+
+# 'Index' url page to get articles from
+url <- html('https://www.nngroup.com/articles/?page=1')
+
+
+# function to get titles of articles
+get_titles <- function(html){
+  html %>% 
+    # The relevant tag
+    html_nodes('#recentTab h3') %>%      
+    html_text() %>% 
+    # Trim additional white space
+    str_trim() %>%                       
+    # Convert the list into a vector
+    unlist()                             
+}
+
+
+# Algorithm: Read through articles page and store titles in a vector.
+    #Read initial URL.
+    url <- html('https://www.nngroup.com/articles/?page=1')
+    
+    # Read list of page numbers. 
+    page_nums <- url %>%
+      html_nodes(".article-pager a") %>%
+      html_text(trim = TRUE)
+    
+    # Get last element in vector. 
+    last_element <- tail(page_nums, n=1)
+    
+    # Initialise vector
+    article_titles <- vector()
+    
+    while(last_element == "» Next"){
+      # Read article titles, using bespoke function. 
+      article_titles <- append(article_titles, get_titles(url))
+      article_titles
+      
+      # Get current page.
+      current_page <- url %>%
+        html_node('.currentpage') %>%
+        html_text(trim = TRUE) %>%
+        as.numeric()
+      
+      # Get next page.
+      next_page <- current_page + 1
+      
+      # Update url.
+      url <- paste0('https://www.nngroup.com/articles/?page=', next_page)
+      
+      # Read list of page numbers. 
+      page_nums <- url %>%
+        html_nodes(".article-pager a") %>%
+        html_text(trim = TRUE)
+      
+      # Get last element in vector. 
+      last_element <- tail(page_nums, n=1)
+    }
