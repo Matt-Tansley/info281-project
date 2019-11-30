a49d5c592bd4c4f4f826908060c2b629d59ead7a
CrunchyPancakes <30matthewt@gmail.com>
2019-11-30 22:54:10 +1300

    Starting to add map markers.
    Markers currently based on AIMS addresses. Next step is to have addresses in the InternetNZ datasets, combining the AIMS data to get coordinates.

--- R Leaflet Tutorial/leaflet-practice/leaflet_script.r
+++ R Leaflet Tutorial/leaflet-practice/leaflet_script.r
@@ -2,10 +2,7 @@
 library(leaflet)
 library(rgdal)
 
-# Create leaflet object.
-m <- leaflet()
-m
-
+# Basic map.
 m <- leaflet() %>%
   # Add base map. 
   addTiles() %>%
@@ -13,21 +10,46 @@ m <- leaflet() %>%
   setView(lat = -40.9006, lng = 174.8860, zoom = 4)
 m
 
-
-# Uses providerTiles to change default map theme. 
-
+# Customised map. 
 m <- leaflet() %>% 
   # Add base map. 
   #addTiles() %>%
-  addProviderTiles(providers$Hydda.Full) %>%
+  # Use an alternative tile set (different theme).
+  addProviderTiles(providers$CartoDB.VoyagerLabelsUnder) %>%
   # Center over NZ, using coordinates.
   setView(lat = -40.9006, lng = 174.8860, zoom = 4) %>%
   # Add polygons, using shapefile to divide by region.
-  addPolygons(data = nz_regions, color = "#FFFFFF", weight = 1, smoothFactor = 0.5,
-              opacity = 1.0, fillOpacity = 0.5,
-              fillColor = 'yellow', label = nz_regions@data$REGC2018_1)
+  # Colour and add labels. 
+  # viridis generates a colour palette of n colours, where
+  # n is the number of rows in the nz_region@data data.frame.
+  addPolygons(data = nz_regions, color = "#FFFFFF", 
+              weight = 1, smoothFactor = 1,
+              opacity = 1, fillOpacity = 1,
+              fillColor = viridis(nrow(nz_regions@data)), 
+              label = nz_regions@data$REGC2018_1)
 m
 
+# Reading shapefiles.
 nz_regions <- readOGR("data/statsnzregional-council-2018-generalised-SHP (1)/regional-council-2018-generalised.shp")
 
 hawkes <- readOGR("C:/Users/30mat/Documents/lds-hawkes-bay-03m-rural-aerial-photos-index-tiles-2014-2015-SHP/hawkes-bay-03m-rural-aerial-photos-index-tiles-2014-2015.shp")
+
+# Reading InternetNZ Data.
+# Unfortunately I cannot make this data public, so it will
+# not be uploaded to GitHub. 
+
+# Reading AIMS Address Position
+address_position <- read_csv("C:/Users/30mat/Documents/VUW/2019/Tri 3/INFO 281 - 391/InternetNZ Data/lds-new-zealand-15layers-CSV/aims-address-position/aims-address-position.csv")
+
+# Adding circle markers. 
+m <- leaflet() %>% 
+  addProviderTiles(providers$CartoDB.VoyagerLabelsUnder) %>%
+  setView(lat = -40.9006, lng = 174.8860, zoom = 4) %>%
+  addPolygons(data = nz_regions, color = "#FFFFFF", 
+              weight = 1, smoothFactor = 1,
+              opacity = 1, fillOpacity = 1,
+              fillColor = viridis(nrow(nz_regions@data)), 
+              label = nz_regions@data$REGC2018_1) %>%
+  addCircleMarkers(lat = address_position$shape_Y, 
+                   lng = address_position$shape_X)
+m
